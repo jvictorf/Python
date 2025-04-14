cb5c9f22a5d435f3ebda6bd0c69666df452e514c
unknown <joovictor999@gmail.com>
2025-04-14 17:12:35 -0300

    arquivos de modulos, libs e read

---
+++ dia_06/06_modulos_lib.py
@@ -0,0 +1,26 @@
+# %%
+import math
+from math import pi, e
+import math as mh
+
+# %%
+math.sqrt(9)
+
+#%%
+math.pow(2, 4)
+
+# %%
+math.e
+
+# %%
+e
+
+# %%
+
+math.pi
+
+# %%
+pi
+
+# %%
+mh.pi
---
+++ dia_06/07_aleatorio.py
@@ -0,0 +1,10 @@
+# %%
+import random
+
+# %%
+random.randint(1, 10)
+
+# %%
+x = ["joão", "Polly", "Rosana", "Maria", "Pedro", "Rafael"]
+
+random.choice(x)
