a764f23870e97d8428bc9c2be14c0e0c9fd41ba0
unknown <joovictor999@gmail.com>
2025-04-28 17:46:19 -0300

    incluso mais um exercicio mais focado em logica

--- exercicios_extras/15_lista_compras.py
+++ exercicios_extras/15_lista_compras.py
@@ -1,7 +1,21 @@
 #%%
-lista = []
-incluir_item = input("Entre com um intem: ")
+lista_compra = []
 
-lista.append(incluir_item)
-
-lista
+while True:
+    incluir_item = input("Entre com um item (ou digite 'remover' para excluir um item): ")
+    
+    if incluir_item == "":
+        print("Lista final:", lista_compra)
+        break
+    elif incluir_item.lower() == "remover":
+        item_remover = input("Digite o nome do item que deseja remover: ")
+        if item_remover in lista_compra:
+            lista_compra.remove(item_remover)
+            print(f"Item '{item_remover}' removido.")
+        else:
+            print(f"Item '{item_remover}' não encontrado na lista.")
+    else:
+        lista_compra.append(incluir_item)
+        print("Item adicionado:", incluir_item)
+    
+    print("Lista atual:", lista_compra)
